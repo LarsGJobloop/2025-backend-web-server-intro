c0fa3ccebf1604ea4708567375fee43c989ccab9
zabronax <zabronax@gmail.com>
2025-01-30 12:25:43 +0100

    implement book listing endpoint

--- Program.cs
+++ Program.cs
@@ -1,30 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+RentingService rentingService = new RentingService();
+
 app.MapGet("/", () =>
 {
-  // return Results.Ok(new { message = "Hello", content = "Testing testing" });
-  var jsonPayload = new { message = "Hello", content = "Testing testing" };
-  return Results.Ok(jsonPayload);
-});
+  var bookInventory = rentingService.ListAllBooks();
+  var booksList = bookInventory.Select(inventoryEntry => inventoryEntry.Key);
 
-app.MapPost("/", (BorrowRequest requestBody) =>
-{
-  Console.WriteLine($"Message: {requestBody.Message}");
-  Console.WriteLine($"Number: {requestBody.Number}");
-
-  return Results.Accepted();
-});
-
-app.MapPut("/{articlId}", (int articlId) =>
-{
-  Console.WriteLine($"At dynamic segment: {articlId}");
-  return Results.Accepted();
-});
-
-app.MapDelete("/", () =>
-{
-  return Results.Created();
+  return Results.Ok(booksList);
 });
 
 app.Run();
