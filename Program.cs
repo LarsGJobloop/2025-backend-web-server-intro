4b951193fe52f56a514394668f9fc8a6ac2d4e42
zabronax <zabronax@gmail.com>
2025-01-30 11:31:41 +0100

    add examples for request and response handling

--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/testing", () => "Testing testing!");
+app.MapGet("/", () =>
+{
+  // return Results.Ok(new { message = "Hello", content = "Testing testing" });
+  var jsonPayload = new { message = "Hello", content = "Testing testing" };
+  return Results.Ok(jsonPayload);
+});
+
+app.MapPost("/", (BorrowRequest requestBody) =>
+{
+  Console.WriteLine($"Message: {requestBody.Message}");
+  Console.WriteLine($"Number: {requestBody.Number}");
+
+  return Results.Accepted();
+});
+
+app.MapPut("/{articlId}", (int articlId) =>
+{
+  Console.WriteLine($"At dynamic segment: {articlId}");
+  return Results.Accepted();
+});
+
+app.MapDelete("/", () =>
+{
+  return Results.Created();
+});
 
 app.Run();
